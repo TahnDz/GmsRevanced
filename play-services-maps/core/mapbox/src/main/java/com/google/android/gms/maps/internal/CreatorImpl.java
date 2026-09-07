71b84f2c082a5837950c466645b839bc196ad509
TahnDz <nguyentuananhn21@gmail.com>
2026-09-07 13:28:48 +0700

    Add GmsCoreRevanced source

---
+++ play-services-maps/core/mapbox/src/main/java/com/google/android/gms/maps/internal/CreatorImpl.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2013-2017 microG Project Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.gms.maps.internal;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.Parcel;
+import android.os.RemoteException;
+import androidx.annotation.Keep;
+import android.util.Log;
+
+import com.google.android.gms.dynamic.IObjectWrapper;
+import com.google.android.gms.dynamic.ObjectWrapper;
+import com.google.android.gms.maps.GoogleMapOptions;
+import com.google.android.gms.maps.StreetViewPanoramaOptions;
+import com.google.android.gms.maps.model.internal.IBitmapDescriptorFactoryDelegate;
+import com.mapbox.mapboxsdk.LibraryLoader;
+
+import org.microg.gms.maps.mapbox.CameraUpdateFactoryImpl;
+import org.microg.gms.maps.mapbox.MapFragmentImpl;
+import org.microg.gms.maps.mapbox.MapViewImpl;
+import org.microg.gms.maps.mapbox.StreetViewPanoramaFragmentImpl;
+import org.microg.gms.maps.mapbox.StreetViewPanoramaViewImpl;
+import org.microg.gms.maps.mapbox.model.BitmapDescriptorFactoryImpl;
+import org.microg.gms.maps.mapbox.utils.MapContext;
+import org.microg.gms.maps.mapbox.utils.MultiArchLoader;
+
+@Keep
+public class CreatorImpl extends ICreator.Stub {
+    private static final String TAG = "GmsMapCreator";
+
+    @Override
+    public void init(IObjectWrapper resources) {
+        initV2(resources, 0);
+    }
+
+    @Override
+    public IMapFragmentDelegate newMapFragmentDelegate(IObjectWrapper activity) {
+        return new MapFragmentImpl(ObjectWrapper.unwrapTyped(activity, Activity.class));
+    }
+
+    @Override
+    public IMapViewDelegate newMapViewDelegate(IObjectWrapper context, GoogleMapOptions options) {
+        return new MapViewImpl(ObjectWrapper.unwrapTyped(context, Context.class), options);
+    }
+
+    @Override
+    public ICameraUpdateFactoryDelegate newCameraUpdateFactoryDelegate() {
+        return new CameraUpdateFactoryImpl();
+    }
+
+    @Override
+    public IBitmapDescriptorFactoryDelegate newBitmapDescriptorFactoryDelegate() {
+        return BitmapDescriptorFactoryImpl.INSTANCE;
+    }
+
+    @Override
+    public void initV2(IObjectWrapper resources, int flags) {
+        Resources mapResources = ObjectWrapper.unwrapTyped(resources, Resources.class);
+        ClassLoader moduleClassLoader = CreatorImpl.class.getClassLoader();
+        if (moduleClassLoader == null) {
+            throw new IllegalStateException("Mapbox backend class loader is not available");
+        }
+        BitmapDescriptorFactoryImpl.INSTANCE.initialize(mapResources, null);
+        MapContext.setModuleResources(mapResources);
+        LibraryLoader.setLibraryLoader(new MultiArchLoader(moduleClassLoader));
+        Log.d(TAG, "initV2 " + flags);
+    }
+
+    @Override
+    public IStreetViewPanoramaViewDelegate newStreetViewPanoramaViewDelegate(IObjectWrapper context, StreetViewPanoramaOptions options) {
+        return new StreetViewPanoramaViewImpl(ObjectWrapper.unwrapTyped(context, Context.class));
+    }
+
+    @Override
+    public IStreetViewPanoramaFragmentDelegate newStreetViewPanoramaFragmentDelegate(IObjectWrapper activity) {
+        return new StreetViewPanoramaFragmentImpl(ObjectWrapper.unwrapTyped(activity, Activity.class));
+    }
+
+    @Override
+    public int getRendererType() throws RemoteException {
+        return 2;
+    }
+
+    @Override
+    public void logInitialization(IObjectWrapper context, int preferredRenderer) throws RemoteException {
+        Log.d(TAG, "Mapbox-based Map initialized (preferred renderer was " + preferredRenderer + ")");
+    }
+
+    @Override
+    public boolean onTransact(int code, Parcel data, Parcel reply, int flags) throws RemoteException {
+        if (super.onTransact(code, data, reply, flags)) return true;
+        Log.d(TAG, "onTransact [unknown]: " + code + ", " + data + ", " + flags);
+        return false;
+    }
+}
---
+++ play-services-maps/core/mapbox/src/main/kotlin/org/microg/gms/maps/mapbox/utils/MultiArchLoader.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019 microG Project Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.microg.gms.maps.mapbox.utils
+
+import android.annotation.SuppressLint
+import android.util.Log
+import com.mapbox.mapboxsdk.LibraryLoader
+import dalvik.system.BaseDexClassLoader
+
+class MultiArchLoader(private val moduleClassLoader: ClassLoader) : LibraryLoader() {
+    @SuppressLint("UnsafeDynamicallyLoadedCode")
+    override fun load(name: String) {
+        try {
+            findLibrary(name)?.let { path ->
+                Log.d(TAG, "Loading $name from $path")
+                System.load(path)
+                return
+            }
+        } catch (e: Exception) {
+            Log.w(TAG, e)
+        }
+        Log.d(TAG, "Loading native $name")
+        System.loadLibrary(name)
+    }
+
+    private fun findLibrary(name: String): String? {
+        return (moduleClassLoader as? BaseDexClassLoader)?.findLibrary(name)
+    }
+
+    companion object {
+        private val TAG = "GmsMultiArchLoader"
+    }
+
+}
